1b4a1753745ed4bcb1a6f586648c6563e38ea069
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-01-25 09:56:41 -0800

    Change dog-related classes to ids as only used once.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
-    $(".dog-showing").toggle();
-    $(".dog-hidden").toggle();
+    $("#dog-showing").toggle();
+    $("#dog-hidden").toggle();
   });
 });
